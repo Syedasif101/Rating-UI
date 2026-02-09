2cbf5819da131abcb0bc1b077deed90811377a6b
Syedasif101 <saasif51120@gmail.com>
2026-02-09 12:57:23 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Rating from "./components/Rating";
+const App = () => {
+  
+  return ( 
+    <>
+    <Rating heading = "How do you feel about React?"/>
+    {/* <Rating heading = "How do you feel about Vue?"/>
+    <Rating heading = "How do you feel about Angular?"/> */}
+    </>
+   );
+}
+ 
+export default App;
